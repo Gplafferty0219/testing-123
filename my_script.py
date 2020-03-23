58a0cec26d3882edb3c30325f492c8c22646a664
Gavin Lafferty <59664725+Gplafferty0219@users.noreply.github.com>
2020-03-23 17:41:01 -0400

    Configured basic function with tests

---
+++ my_script.py
@@ -0,0 +1,9 @@
+def enlarge(i):
+    return i * 100
+
+
+
+if __name__=="__main__":
+    my_number = float(input("choose a number: "))
+    n=enlarge(my_number)
+    print("enlarging the number:",n)
